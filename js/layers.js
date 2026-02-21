577bc2af7c5fdaf2e0374d40c229647cef58a6b1
liamkelly4123-jpg <liamkelly4123@gmail.com>
2026-02-21 08:56:04 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -21,18 +21,18 @@ addLayer("1layer", {
 
 addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id
-    symbol: "points", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "prestige", // This appears on the layer's node. Default is the id with the first letter capitalized
     symbolI18N: "Prestige", // Second name of symbol for internationalization (i18n) if internationalizationMod is enabled
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "test points", // Name of prestige currency
-    resourceI18N: "test points", // Second name of the resource for internationalization (i18n) if internationalizationMod is enabled
+    resource: "prestige points", // Name of prestige currency
+    resourceI18N: "prestige points", // Second name of the resource for internationalization (i18n) if internationalizationMod is enabled
     baseResource: "points", // Name of resource prestige is based on
     baseResourceI18N: "points", // Second name of the baseResource for internationalization (i18n) if internationalizationMod is enabled
     baseAmount() {return player.points}, // Get the current amount of baseResource
